c654c9da8feaa0ff8fa50b75a1ccdb35e89c74e2
Your Name <you@example.com>
2020-12-12 15:15:16 +0100

    init

--- main.py
+++ main.py
@@ -2,17 +2,29 @@ import requests
 import pandas as pd
 import tablib
 import openpyxl
+from datetime import date
+import json
 
-url = "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/pl/pln/pl-PL/KRK-sky/STN-sky/2021-01-01"
+
+ldn_airports = ["LHR-sky", "STN-sky", "LGW-sky", "LTN-sky"]
+ldn_arr = []
+today = str(date.today())
 
 headers = {
     'x-rapidapi-key': "422894374fmsh023eb926213aa23p1b8967jsn96e86f739667",
     'x-rapidapi-host': "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com"
     }
 
-response = requests.request("GET", url, headers=headers)
+for i in ldn_airports:
+  x = "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/pl/pln/pl-PL/KRK-sky/" + i + "/" + today
+  ldn_arr.append(x)
 
-data = pd.DataFrame.from_dict(response)
-print(data)
+for i in ldn_arr:
+	response = requests.request("GET", i, headers=headers)
+	response_data = response.json()
+	print(response_data)
 
-data.to_excel("loty.xlsx")
+
+# data.to_csv('flights.csv')
+print('json END')
+
