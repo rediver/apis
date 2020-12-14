c3aac9f0055d5afef8047fb67f2114b543d0e2e8
Your Name <you@example.com>
2020-12-14 11:09:19 +0100

    add handler()

--- main.py
+++ main.py
@@ -5,26 +5,27 @@ import openpyxl
 from datetime import date
 import json
 
+def handler(): 
 
-ldn_airports = ["LHR-sky", "STN-sky", "LGW-sky", "LTN-sky"]
-ldn_arr = []
-today = str(date.today())
+	ldn_airports = ["LHR-sky", "STN-sky", "LGW-sky", "LTN-sky"]
+	ldn_arr = []
+	today = str(date.today())
 
-headers = {
-    'x-rapidapi-key': "422894374fmsh023eb926213aa23p1b8967jsn96e86f739667",
-    'x-rapidapi-host': "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com"
-    }
+	headers = {
+	    'x-rapidapi-key': "422894374fmsh023eb926213aa23p1b8967jsn96e86f739667",
+	    'x-rapidapi-host': "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com"
+	    }
 
-for i in ldn_airports:
-  x = "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/pl/pln/pl-PL/KRK-sky/" + i + "/" + today
-  ldn_arr.append(x)
+	for i in ldn_airports:
+	  x = "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/pl/pln/pl-PL/KRK-sky/" + i + "/" + today
+	  ldn_arr.append(x)
 
-for i in ldn_arr:
-	response = requests.request("GET", i, headers=headers)
-	response_data = response.json()
-	print(response_data)
+	for i in ldn_arr:
+		response = requests.request("GET", i, headers=headers)
+		response_data = response.json()
+		print(response_data)
 
 
-# data.to_csv('flights.csv')
+handler()
 print('json END')
 
